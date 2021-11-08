f0ecbd64a09fb909fbbb72f7ff3fa3cc917caff1
Teresa Gao <t_gao@mit.edu>
2021-11-08 11:18:06 -0500

    add Tensorflow AlexNet skeleton for main.py

--- repos/grayscale-CNN/main.py
+++ repos/grayscale-CNN/main.py
@@ -1,78 +1,40 @@
-import numpy as np
-import keras.backend as K
+import tensorflow as tf
 import matplotlib.pyplot as plt
+from tensorflow.keras import datasets, layers, models, losses
 
-from keras import layers
-from keras.layers import Input, Dense, Activation,BatchNormalization, Flatten, Conv2D, MaxPooling2D
-from keras.models import Model
-from keras.preprocessing import image
-from keras.preprocessing.image import ImageDataGenerator
-from matplotlib.pyplot import imshow
+# TODO: load and split train/test/validation data
 
-def AlexNet(input_shape): # from https://medium.com/analytics-vidhya/multi-class-image-classification-using-alexnet-deep-learning-network-implemented-in-keras-api-c9ae7bc4c05f
-
-    X_input = Input(input_shape)
-
-    X = Conv2D(96,(11,11),strides = 4,name="conv0")(X_input)
-    X = BatchNormalization(axis = 3 , name = "bn0")(X)
-    X = Activation('relu')(X)
-
-    X = MaxPooling2D((3,3),strides = 2,name = 'max0')(X)
-
-    X = Conv2D(256,(5,5),padding = 'same' , name = 'conv1')(X)
-    X = BatchNormalization(axis = 3 ,name='bn1')(X)
-    X = Activation('relu')(X)
-
-    X = MaxPooling2D((3,3),strides = 2,name = 'max1')(X)
-
-    X = Conv2D(384, (3,3) , padding = 'same' , name='conv2')(X)
-    X = BatchNormalization(axis = 3, name = 'bn2')(X)
-    X = Activation('relu')(X)
-
-    X = Conv2D(384, (3,3) , padding = 'same' , name='conv3')(X)
-    X = BatchNormalization(axis = 3, name = 'bn3')(X)
-    X = Activation('relu')(X)
-
-    X = Conv2D(256, (3,3) , padding = 'same' , name='conv4')(X)
-    X = BatchNormalization(axis = 3, name = 'bn4')(X)
-    X = Activation('relu')(X)
-
-    X = MaxPooling2D((3,3),strides = 2,name = 'max2')(X)
-
-    X = Flatten()(X)
-
-    X = Dense(4096, activation = 'relu', name = "fc0")(X)
-
-    X = Dense(4096, activation = 'relu', name = 'fc1')(X)
-
-    X = Dense(6,activation='softmax',name = 'fc2')(X)
-
-    model = Model(inputs = X_input, outputs = X, name='AlexNet')
+def build_model(layers):
+    model = models.Sequential()
+    for layer in layers:
+        model.add(layer)
     return model
 
 if __name__ == '__main__':
-    seed = 22
-    K.set_image_data_format(‘channels_last’)
+    alexnet = build_model([
+        layers.experimental.preprocessing.Resizing(224, 224, interpolation="bilinear", input_shape=x_train.shape[1:])
 
-    path = '/Volumes/imaging_analysis/2021_10_06_HumanProteinAtlas_CiminiLab_TeresaGao/Greyscale_Images/' # must be connected to Broad Institute IP group VPN
-    train_datagen = ImageDataGenerator(
-        label_mode='categorical',
-        image_size=(2048, 2048),
-        rescale=1./255,
-        seed=seed,
-    )
-    train = train_datagen.flow_from_directory(
-        path,
-        target_size=(2048,2048),
-        class_mode='categorical',
-        seed=seed,
-    )
+        layers.Conv2D(96, 11, strides=4, padding='same')
+        layers.Lambda(tf.nn.local_response_normalization)
+        layers.Activation('relu')
+        layers.MaxPooling2D(3, strides=2)
 
-    alex = AlexNet((2048, 2048))
-    alex.compile(
-        optimizer = 'adam',
-        loss = 'categorical_crossentropy',
-        metrics=['accuracy'],
-    )
+        layers.Conv2D(384, 3, strides=4, padding='same')
+        layers.Activation('relu')
 
-    alex.fit_generator(train, epochs=50)
+        layers.Conv2D(384, 3, strides=4, padding='same')
+        layers.Activation('relu')
+
+        layers.Flatten()
+        layers.Dense(4096, activation='relu')
+        layers.Dropout(0.5)
+
+        layers.Dense(10, activation='softmax')
+    ])
+
+    print(alexnet.summary())
+
+    alexnet.compile(optimizer='adam', loss=losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+
+    # TODO: train/test
+    # TODO: validate
