f517c75ee4c3dc06cff09f625dc0bf1f60e55723
Teresa Gao <t_gao@mit.edu>
2022-05-09 21:02:14 -0500

    create separate .sh and .sub files for when dataset is VOC in config

---
+++ PANet/config-voc.py
@@ -0,0 +1,183 @@
+"""Experiment Configuration"""
+import os
+import re
+import glob
+import itertools
+
+import sacred
+from sacred import Experiment
+from sacred.observers import FileStorageObserver
+from sacred.utils import apply_backspaces_and_linefeeds
+
+import neptune.new as neptune
+from neptune.new.integrations.tensorflow_keras import NeptuneCallback
+from neptune.new.types import File
+
+# initialize Neptune.ai with API token
+with open('../../neptune-api-token.txt', 'r') as f:
+    run = neptune.init(
+        api_token=f.read(),
+        project='ip-superurop-tgao'
+    )
+
+sacred.SETTINGS['CONFIG']['READ_ONLY_CONFIG'] = False
+sacred.SETTINGS.CAPTURE_MODE = 'no'
+
+ex = Experiment('PANet')
+ex.captured_out_filter = apply_backspaces_and_linefeeds
+
+source_folders = ['.', './dataloaders', './models', './util']
+sources_to_save = list(itertools.chain.from_iterable(
+    [glob.glob(f'{folder}/*.py') for folder in source_folders]))
+for source_file in sources_to_save:
+    ex.add_source_file(source_file)
+
+@ex.config
+def cfg():
+    """Default configurations"""
+    input_size = (256, 256)
+    seed = 1234
+    cuda_visable = '0, 1, 2, 3, 4, 5, 6, 7'
+    gpu_id = 0
+    mode = 'train' # 'train' or 'test'
+
+    parameters = {
+        'input_size': input_size,
+        'seed': seed,
+        'cuda_visable': cuda_visable,
+        'gpu_id': gpu_id,
+        'mode': mode,
+    }
+
+    if mode == 'train':
+        dataset = 'VOC' # TODO: toggle back to 'HPA'  # 'VOC' or 'COCO' or 'HPA'
+        n_steps = 30000
+        label_sets = 0
+        batch_size = 1
+        lr_milestones = [10000, 20000, 30000]
+        align_loss_scaler = 1
+        ignore_label = 255
+        print_interval = 100
+        save_pred_every = 10000
+
+        model = {
+            'align': True,
+        }
+
+        task = {
+            'n_ways': 1,
+            'n_shots': 1, # TODO: toggle between 1 and 5
+            'n_queries': 1,
+        }
+
+        optim = {
+            'lr': 1e-3,
+            'momentum': 0.9,
+            'weight_decay': 0.0005,
+        }
+
+        parameters.update({
+            'dataset': dataset,
+            'n_steps': n_steps,
+            'label_sets': label_sets,
+            'batch_size': batch_size,
+            'lr_milestones': lr_milestones,
+            'align_loss_scaler': align_loss_scaler,
+            'ignore_label': ignore_label,
+            'print_interval': print_interval,
+            'save_pred_every': save_pred_every,
+            'model': model,
+            'task': task,
+            'optim': optim,
+        })
+
+    elif mode == 'test':
+        notrain = False
+        snapshot = './runs/PANet_HPA_sets_0_1way_1shot_[train]/1/snapshots/30000.pth'
+        n_runs = 5
+        n_steps = 1000
+        batch_size = 1
+        scribble_dilation = 0
+        bbox = False
+        scribble = False
+
+        # Set dataset config from the snapshot string
+        if 'VOC' in snapshot:
+            dataset = 'VOC'
+        elif 'COCO' in snapshot:
+            dataset = 'COCO'
+        elif 'HPA' in snapshot:
+            dataset = 'HPA'
+        else:
+            raise ValueError('Wrong snapshot name !')
+
+        # Set model config from the snapshot string
+        model = {}
+        for key in ['align',]:
+            model[key] = key in snapshot
+
+        # Set label_sets from the snapshot string
+        label_sets = int(snapshot.split('_sets_')[1][0])
+
+        # Set task config from the snapshot string
+        task = {
+            'n_ways': int(re.search("[0-9]+way", snapshot).group(0)[:-3]),
+            'n_shots': int(re.search("[0-9]+shot", snapshot).group(0)[:-4]),
+            'n_queries': 1,
+        }
+
+        parameters.update({
+            'n_runs': n_runs,
+            'n_steps': n_steps,
+            'batch_size': batch_size, 
+            'scribble_dilation': scribble_dilation,
+            'bbox': bbox,
+            'scribble': scribble,
+            'dataset': dataset,
+            'model': model,
+            'label_sets': label_sets,
+            'task': task,
+        })
+
+    else:
+        raise ValueError('Wrong configuration for "mode" !')
+
+
+    exp_str = '_'.join(
+        [dataset,]
+        + [key for key, value in model.items() if value]
+        + [f'sets_{label_sets}', f'{task["n_ways"]}way_{task["n_shots"]}shot_[{mode}]'])
+
+
+    path = {
+        'log_dir': './runs',
+        'init_path': './pretrained_model/vgg16-397923af.pth',
+        'VOC':{'data_dir': '../../data/Pascal/VOCdevkit/VOC2012/',
+               'data_split': 'trainaug',},
+        'COCO':{'data_dir': '../../data/COCO/',
+                'data_split': 'train',},
+        'HPA':{'data_dir': '../../data/',
+                'rgb_dir': 'PbMgNr_jpg',
+                'grayscale_dir': 'Greyscale_Images_png',
+                'data_split': '/' }
+    }
+
+    # log to Neptune
+    parameters['exp_str'] = exp_str
+    parameters['path'] = path
+    run['config/parameters'] = parameters
+
+@ex.config_hook
+def add_observer(config, command_name, logger):
+    """A hook fucntion to add observer"""
+    exp_name = f'{ex.path}_{config["exp_str"]}'
+    if config['mode'] == 'test':
+        if config['notrain']:
+            exp_name += '_notrain'
+        if config['scribble']:
+            exp_name += '_scribble'
+        if config['bbox']:
+            exp_name += '_bbox'
+    observer = FileStorageObserver.create(os.path.join(config['path']['log_dir'], exp_name))
+    ex.observers.append(observer)
+    return config
